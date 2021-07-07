983bb962cdafa208565a550235b22671e1bb927e
godrowr <godrowr@protonmail.com>
2021-07-07 14:46:52 -0400

    First implementation

--- go-crawler.go
+++ go-crawler.go
@@ -13,8 +13,12 @@ import (
 	"golang.org/x/net/html"
 )
 
+var links = make(map[string]string)
+
+var host string
+
 func main() {
-	fmt.Println("Enter site to crawl:")
+	fmt.Println("Enter site to crawl (ex: http://www.rescale.com/):")
 	reader := bufio.NewReader(os.Stdin)
 	input, err := reader.ReadString('\n')
 	if err != nil {
@@ -27,19 +31,36 @@ func main() {
 		fmt.Println("String inputted was not a URL", err)
 		return
 	}
-	crawl(u)
+	host = u.Hostname()
+	loop(u.String())
+
 }
 
-func crawl(input *url.URL) {
-	fmt.Println(input.String())
-	resp, err := http.Get(input.String())
+func loop(input string) {
+	crawl(input)
+	for URL, visited := range links { //TODO
+		if visited == "Not-Visited" && strings.Contains(URL, host) {
+			links[URL] = "Visited"
+			crawl(URL)
+		} else {
+			links[URL] = "Visited"
+		}
+	}
+	for URL, visited := range links {
+		if visited == "Not-Visited" {
+			loop(URL)
+		}
+	}
+	os.Exit(0)
+}
+
+func crawl(input string) {
+	fmt.Println(input)
+	links[input] = "Visited"
+	resp, err := http.Get(input)
 	if err != nil {
 		fmt.Println("GET request could not be processed correctly", err)
 		return
-	}
-	base, err := url.Parse(input.String())
-	if err != nil {
-		log.Fatal(err)
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
@@ -49,29 +70,37 @@ func crawl(input *url.URL) {
 	}
 
 	doc, err := html.Parse(strings.NewReader(string(body)))
-	var f func(*html.Node)
-	f = func(n *html.Node) {
-		if n.Type == html.ElementNode && n.Data == "a" {
-			for _, a := range n.Attr {
-				if a.Key == "href" && len(a.Val) > 0 {
-					url, err := url.ParseRequestURI(a.Val)
-					if err != nil {
-						log.Fatal(err)
-					}
-					URL := base.ResolveReference(url).String()
-					if url.Scheme != "tel" && strings.Compare(URL, input.String()) != 0 {
-						fmt.Println(" " + URL)
-					}
-					break
-				}
+	if err != nil {
+		fmt.Println("DOC could not be obtained from BODY", err)
+		return
+	}
+	f(doc, input)
+}
 
+func f(n *html.Node, input string) {
+	base, err := url.Parse(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, a := range n.Attr {
+			if a.Key == "href" && len(a.Val) > 0 {
+				url, err := url.ParseRequestURI(a.Val)
+				if err == nil {
+					URL := base.ResolveReference(url).String()
+					if url.Scheme != "tel" && strings.Compare(URL, input) != 0 {
+						if links[URL] == "" || links[URL] != "Visited" {
+							links[URL] = "Not-Visited"
+						}
+						fmt.Println(" " + URL + " " + links[URL])
+					}
+				}
+				break
 			}
-		}
-		for c := n.FirstChild; c != nil; c = c.NextSibling {
-			f(c)
+
 		}
 	}
-	f(doc)
-
-	//http://www.rescale.com/
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		f(c, input)
+	}
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/godrowr/go-crawler
 go 1.16
 
 require (
+	github.com/jackdanger/collectlinks v0.0.0-20160421202702-24c4ee2870ba
 	golang.org/x/net v0.0.0-20210614182718-04defd469f4e
 	mvdan.cc/xurls v1.1.0
 	rsc.io/quote v1.5.2 // indirect
